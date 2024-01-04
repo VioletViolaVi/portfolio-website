250d6d04c1526d547274142be7edd72f40e87236
VioletViolaVi <62072827+VioletViolaVi@users.noreply.github.com>
2024-01-04 23:34:02 +0000

    Add js to change burger icon to cross icon

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,19 @@
 $(document).ready(function () {
 
-    // toggles small navbar & overlay
+    // toggles small navbar & overlay - using burger icon
     $(".fa-solid.fa-bars").on("click", () => {
         $(".links").slideToggle("slow");
         $(".overlay").fadeToggle("slow");
+        $(".fa-solid.fa-bars").toggleClass("hide-icon");
+        $(".fa-solid.fa-xmark").toggleClass("hide-icon");
+    });
+
+    // toggles small navbar & overlay - using cross icon
+    $(".fa-solid.fa-xmark").on("click", () => {
+        $(".links").slideToggle("slow");
+        $(".overlay").fadeToggle("slow");
+        $(".fa-solid.fa-xmark").toggleClass("hide-icon");
+        $(".fa-solid.fa-bars").toggleClass("hide-icon");
     });
 
     // closes small navbar from overlay
